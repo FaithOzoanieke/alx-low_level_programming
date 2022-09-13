2487c3008889eea0cc322b9aa06656ca821616b1
FaithOzoanieke <faithozoanieke@gmail.com>
2022-09-13 15:59:44 -0700

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	putchar('');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char a[] = "_putchar";
+	int b;
+
+	for (b = 0; b < a; b++)
+	{
+		_putchar(a[b]);
+	}
 	_putchar('\n');
-	return(0);
+
+	return (0);
 }
