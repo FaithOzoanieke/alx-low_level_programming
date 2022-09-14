e5ea9932c3cce8cab00c973c9be833046d1bab49
FaithOzoanieke <faithozoanieke@gmail.com>
2022-09-13 18:54:35 -0700

    Each new term in the Fibonacci sequence is generated by adding the previous two terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,24 +5,49 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
+
 	int i;
 
 	unsigned long int j, k, next, sum;
+
+
+
 	j = 1;
+
 	k = 2;
+
 	sum = 0;
+
+
+
 	for (i = 1; i <= 33; ++i)
+
 	{
+
 		if (j < 4000000 && (j % 2) == 0)
+
 		{
+
 			sum = sum + j;
+
 		}
 
 		next = j + k;
+
 		j = k;
+
 		k = next;
+
 	}
+
+
+
 	printf("%lu\n", sum);
+
+
+
 	return (0);
+
 }
