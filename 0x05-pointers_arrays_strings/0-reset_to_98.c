d26bcd4873ab4d883a589ddfba12ffe34e05a920
FaithOzoanieke <faithozoanieke@gmail.com>
2022-09-19 12:22:49 -0700

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * var_reset - reset the value of the pointer to 98
+ * @n: a pointer to reset 98
+ *
+ * Return void
+ */
+
+void var_reset(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a readme file containing solutions to 0x05-pointers arrays strings task
