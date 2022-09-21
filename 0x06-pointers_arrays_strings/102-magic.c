52ba705d54c29357dcae19e618c5dbf03dbb1b24
FaithOzoanieke <faithozoanieke@gmail.com>
2022-09-21 16:58:20 -0700

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	int n;
@@ -18,7 +24,7 @@ int main(void)
    * - you are not allowed to code anything else than this line of code
    */
 
-*(p + 5) = 98;
+	*(p + 5) = 98;
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
