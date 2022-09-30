92859d701ed9777dc34d369a0b0d4a0148f12325
FaithOzoanieke <faithozoanieke@gmail.com>
2022-09-30 08:07:19 -0700

    Write a program that prints the number of arguments passed into it.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,7 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[] __attributes__((unusedInfo)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
