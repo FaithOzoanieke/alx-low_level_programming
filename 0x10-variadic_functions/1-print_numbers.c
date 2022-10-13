e6731213df4de82299e60bc7933c509d3baec78d
FaithOzoanieke <faithozoanieke@gmail.com>
2022-10-13 14:59:16 -0700

    Write a function that prints numbers, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,9 +1,9 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers given as parameters in the program
- * @separator: string to be printed between numbers in the program
- * @n: number of integers passed to the function in the program
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
